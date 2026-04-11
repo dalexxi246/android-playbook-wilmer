7607702be10c9399c49ac2ea7323646a987bd9a9
Wilmer Hurtado <walex6hh@gmail.com>
2026-04-11 13:12:41 -0500

    chore(project): relax commitlint body line length constraints
    The commit updates the linting configuration to allow commit message bodies with unlimited line lengths:
    - Updated `commitlint.config.mjs` to set the `body-max-line-length` rule to `infinity`.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,6 @@ export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
         "header-max-length": [0, "always", 150],
+        "body-max-line-length": [0, "always", "infinity"],
     },
 };
